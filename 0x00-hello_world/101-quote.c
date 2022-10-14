f306e213e82cc5d8b12aa1289581e1fe8cd898ce
Kofi Sey <mrbennysimpson.kss@gmail.com>
2022-10-14 19:24:50 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print without using printf or puts
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
