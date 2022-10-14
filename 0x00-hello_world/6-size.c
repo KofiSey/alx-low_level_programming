94545d005ea8b1e9a85177158ef2bd2cb0b32b22
Kofi Sey <mrbennysimpson.kss@gmail.com>
2022-10-14 20:41:53 +0000

    making amends

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("Size of a int: %i byte(S)\n", sizeof(int));
 	printf("Size of a long int: %i byte(S)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(S)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(S)\n", sizeof(float));
+	printf("Size of a float: %i byte(S)\n", sizeof(float));
 	return (0);
 }
