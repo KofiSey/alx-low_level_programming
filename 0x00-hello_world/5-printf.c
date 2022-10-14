15c51f2ee865d41ac86d584828f98724e9071cc4
Kofi Sey <mrbennysimpson.kss@gmail.com>
2022-10-14 15:40:21 +0000

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printf function to print a line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
