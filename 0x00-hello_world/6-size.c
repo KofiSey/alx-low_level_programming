7db5784ccc2710cd7cc26cf256334598600e2db8
Kofi Sey <mrbennysimpson.kss@gmail.com>
2022-10-14 18:40:10 +0000

    updating the files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/**
+/** 
  * main - finding the size of type
- *
  * Return: 0
- */
+*/
 int main(void)
 {
 	char a;
@@ -13,7 +12,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
-	ptintf("Size of a int: %i byte(S)\n", (unsigned long)sizeof(b));
+	printf("Size of a int: %i byte(S)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %li byte(S)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
