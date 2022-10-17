522b1915d48754067ecba08577fd03509ad24623
Kofi Sey <mrbennysimpson.kss@gmail.com>
2022-10-17 23:13:17 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar('n\');
+	}
+
+	return (0);
+}
