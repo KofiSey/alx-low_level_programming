6d1108c29839326b88e975e54375d8e150560c78
Kofi Sey <mrbennysimpson.kss@gmail.com>
2022-10-21 19:17:27 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - print without using printf or puts
- *
- * Return: 1
- */
+* main - main function in the task
+*
+*
+*Return: return 1
+*/
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
